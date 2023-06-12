4ee84d8dec1a2aa4b664617eb4e7c6e79891a0fb
Nikita Tsvetkov <tsv1@fastmail.com>
2023-06-12 21:50:48 +0400

    bump version → 0.2.0

--- vedro_jj/__init__.py
+++ vedro_jj/__init__.py
@@ -1,6 +1,6 @@
 from ._interceptor import Interceptor, InterceptorType
 from ._vedro_jj import VedroJJ, VedroJJPlugin, interceptor
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __all__ = ("VedroJJ", "VedroJJPlugin", "interceptor",
            "Interceptor", "InterceptorType",)
